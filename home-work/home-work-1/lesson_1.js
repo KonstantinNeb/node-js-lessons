18592eb85bd28fa24e8d365af5b6137961b90435
konstantin-neb <kostyneb36@gmail.com>
2021-10-28 01:30:36 +0300

    home-work-2

--- home-work/home-work-1/lesson_1.js
+++ home-work/home-work-1/lesson_1.js
@@ -1,51 +1,43 @@
 const colors = require('colors');
 
-const getSimpleNum = (min, n) => {
-  if (isNaN(n) || isNaN(min)) return console.log('Аргумент не является числом');
+let n;
+let min;
 
-  for (let i = 2; i <= n; i++) {
-    let flag = 1;
-    if (i > 2 && i % 2 !== 0)
-    {
-      for (let j = 3; j * j <= i ; j = j + 2)
-      {
-        if (i % j === 0)
-        {
-          flag = 0;
-          break;
-        }
-      }
-    }
-    else if (i !== 2) flag = 0;
-    if (flag === 1) {
+if(process.argv[3]) {
+  if (+process.argv[2] > +process.argv[3] ) {
+    n = +process.argv[2];
+    min = +process.argv[3];
+  } else {
+    n = +process.argv[3];
+    min = +process.argv[2];
+  }
+} else {
+  n = +process.argv[2]
+}
 
-      let simpleNum = [];
-      let notSimpleNum = [];
+if(isNaN(n) || isNaN(min)) return console.log(colors.red('Указаны неверные границы диапазон'));
 
-      for (let i = 2; i <= n; i++) {
-        if (notSimpleNum.includes(i)) continue;
+let simpleNum = [];
+let notSimpleNum = [];
 
-        simpleNum.push(i);
+for(let i = 2; i <= n; i++) {
+  if(notSimpleNum.includes(i)) continue;
 
-        if (i <= Math.sqrt(n)) {
-          for (let j = 2; j <= n; j++) {
-            notSimpleNum.push(i*j)
-          }
-        }
-      }
+  simpleNum.push(i);
 
-      let results = simpleNum.filter((item) => min <= item);
-
-      if (results.length === 0) return console.log(colors.red('В указанном диапазоне нет простых чисел'));
-
-      for (let i = 0; i < results.length; i = i + 3) {
-        console.log(colors.green(results[i]));
-        if (results[i + 1]) console.log(colors.yellow(results[i + 1]));
-        if (results[i + 2]) console.log(colors.red(results[i + 2]));
-      }
-      break;
+  if(i <= Math.sqrt(n)) {
+    for(let j = 2; j <= n; j++) {
+      notSimpleNum.push(i*j)
     }
   }
 }
 
-getSimpleNum();
+let results = simpleNum.filter((item) => min <= item)
+
+if(results.length === 0) return console.log(colors.red('В указанном диапазоне нет простых чисел'));
+
+for(let i = 0; i < results.length; i=i+3) {
+  console.log(colors.green(results[i]));
+  if(results[i+1]) console.log(colors.yellow(results[i+1]));
+  if(results[i+2]) console.log(colors.red(results[i+2]));
+}
